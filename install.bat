7602f4e53a69f35b4ec99bc6422155e909ec58f8
Flo3376 <Flo3376@users.noreply.github.com>
2022-03-03 15:13:00 +0100

    Update install.bat
    création d'un installateur btach

--- install.bat
+++ install.bat
@@ -8,3 +8,5 @@ echo Connexion au dépot principale
 git remote add origin https://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@github.com/OmegaFactory/mod.git
 echo Récupération du fichier du dépot
 git pull origin main --allow-unrelated-histories
+
+exit
